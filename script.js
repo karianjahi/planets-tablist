97c1ea99d8159f8143b3d99195b05e0106fbebad
karianjahi <nkarianjahi@gmail.com>
2026-02-03 10:10:52 +0100

    Add event listener and make sure all tabs are de-selected using aria before selecting

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 const tabs = document.querySelectorAll('[role="tab"]');
 const panels = document.querySelectorAll('[role=tabpanel]');
+
+tabs.forEach(tab => {
+    tab.addEventListener("click", () => {
+        tabs.forEach(tb => {
+            tb.setAttribute("aria-selected", "false");
+        });
+    });
+});
